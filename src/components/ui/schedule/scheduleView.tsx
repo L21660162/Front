2f23ad25a0a0950e091403d444b8c45eba84fcc8
Kevin <kalanfr2@gmail.com>
2025-01-22 15:45:55 -0600

    Update

--- src/components/ui/schedule/scheduleView.tsx
+++ src/components/ui/schedule/scheduleView.tsx
@@ -4,34 +4,153 @@ import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { useTranslation } from 'react-i18next';
 import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
-import { ISchedule, useGetAllSchedulesQuery } from '../../../graphql/graphql';
+import { Controller, useForm } from 'react-hook-form';
+import { AutoComplete } from 'primereact/autocomplete';
+import { classNames } from 'primereact/utils';
+import {
+  IRoles,
+  ISchedule,
+  useGetAllSchedulesQuery,
+  useGetAllUsersQuery,
+} from '../../../graphql/graphql';
 import { useAccessTokenData } from '../../../store/auth/store';
 import { GRAPHQL_CLIENT } from '../../../utils/graphqlClient';
+import { TokenData } from '../../../store/auth/type';
+
+interface ITeacherSearchResult {
+  _id?: string | null | undefined;
+  fullName?: string;
+}
 
 function ScheduleView() {
+  const { _id: userId } = useAccessTokenData() as TokenData;
   const { t } = useTranslation('common');
   const [globalFilterValue, setGlobalFilterValue] = useState('');
   const [filteredValue, setFilteredValue] = useState<ISchedule[] | null>(null);
   const [layout, setLayout] = useState('grid');
-  const [sortOrder, setSortOrder] = useState<0 | 1 | -1 | null>(null);
-  const [sortField, setSortField] = useState('createdAt');
-  const [visibleDetails, setVisibleDetails] = useState(false);
-  const [visibleEditForm, setVisibleEditForm] = useState(false);
-  const [visibleDeleteConfirm, setVisibleDeleteConfirm] = useState(false);
+  // const [sortOrder, setSortOrder] = useState<0 | 1 | -1 | null>(null);
+  // const [sortField, setSortField] = useState('createdAt');
+  // const [visibleDetails, setVisibleDetails] = useState(false);
+  // const [visibleEditForm, setVisibleEditForm] = useState(false);
+  // const [visibleDeleteConfirm, setVisibleDeleteConfirm] = useState(false);
 
-  const [selectedSchedule, setSelectedSchedule] = useState<ISchedule | null>(null);
-  const [schedules, setSchedules] = useState<{ [key: string]: string }>({});
+  // const [selectedSchedule, setSelectedSchedule] = useState<ISchedule | null>(null);
+  // const [schedules, setSchedules] = useState<{ [key: string]: string }>({});
+  const [teacherSelectId, setTeacherSelectedId] = useState<any>(undefined);
+  const [teacherSearchResult, setTeacherSearchResult] = useState<
+    ITeacherSearchResult[] | undefined
+  >([]);
+  // const [selectedTeacherInfo, setSelectedTeacherInfo] = useState<ITeacherSearchResult | undefined>(
+  //   undefined
+  // );
+  // const [filterTeacher, setFilterTeacher] = useState<string[] | undefined>([]);
+  const [filterstudents, setFilterStudents] = useState<string[] | undefined>([]);
+  const [code, setCode] = useState<string>('');
 
-  const { data: allScheduleData } = useGetAllSchedulesQuery(GRAPHQL_CLIENT, {
+  const {
+    data: allTeacherData,
+    isFetching,
+    refetch,
+  } = useGetAllUsersQuery(GRAPHQL_CLIENT, {
     limit: 500,
     page: 1,
     offset: 0,
-    // filter: {
-    //   teacher: '678fdf61e5dab4d5b006401d',
-    // },
+    filter: {
+      roles: [IRoles.Docente],
+    },
   });
 
-  
+  const { data: allScheduleData, refetch: requestUserById } = useGetAllSchedulesQuery(
+    GRAPHQL_CLIENT,
+    {
+      limit: 500,
+      page: 1,
+      offset: 0,
+      filter: {
+        teacher: teacherSelectId, //'678fdf61e5dab4d5b006401d',
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (!isFetching) {
+      setTeacherSearchResult(
+        allTeacherData?.getAllUsers.docs.map(({ _id, firstName, lastName, middleName, rfc }) => ({
+          _id,
+          code: rfc,
+          fullName: `${firstName} ${lastName} ${middleName}`,
+        }))
+      );
+    }
+  }, [allTeacherData?.getAllUsers.docs, isFetching]);
+
+  const search = (event) => {
+    setTimeout(() => {
+      let query
+      if (!event.query.trim().length) {
+        query = [...teacherSearchResult];
+      } else {
+        query = teacherSearchResult.filter((teacher) => {
+          return (
+            teacher.fullName.toLowerCase().includes(event.query.toLowerCase()) ||
+            teacher.code.toLowerCase().includes(event.query.toLowerCase())
+          )
+        });
+      }
+      setFilterStudents(query);
+    }, 250);
+    setCode(event.query);
+    refetch();
+  };
+
+  const defaultValues = {
+    teacherName: '',
+  };
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    getValues,
+    reset,
+  } = useForm({ defaultValues });
+
+  const onSubmit = (data: any) => {
+    const selectedStudent = data.teacherName;
+    if (selectedStudent) {
+      setTeacherSelectedId(selectedStudent._id);
+    }
+    requestUserById();
+  };
+
+  const dataTemplate = (item: any) => {
+    return (
+      <div className="p-d-flex p-ai-center p-jc-space-between">
+        <div>
+          <span className="p-text-bold">{item.fullName}</span>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    if (!teacherSelectId) {
+      setTeacherSelectedId(userId);
+    }
+  }, [userId]);
+
+  // useEffect(() => {
+  //   if (StudentServiceStatusData) {
+  //     const { firstName, lastName, middleName, processStep } = StudentServiceStatusData.getUserById;
+  //     const selectedStudent: IStudentSearchResult = {
+  //       fullName: `${firstName} ${lastName} ${middleName}`,
+  //     };
+
+  //     setSelectedStudentInfo(selectedStudent);
+  //     setActiveIndex(processStep as number);
+  //   }
+  // }, [StudentServiceStatusData]);
+
   // useEffect(() => {
   //   if (allScheduleData && allScheduleData.getAllCareers) {
   //     if (Array.isArray(allScheduleData.getAllCareers.docs)) {
@@ -66,21 +185,7 @@ function ScheduleView() {
   //   }
   // };
 
-  console.log('Hola', allScheduleData);
 
-  const dataViewHeader = (
-    <div className="flex flex-column md:flex-row md:justify-content-between gap-2">
-      <span className="p-input-icon-left">
-        <i className="pi pi-search" />
-        <InputText
-          value={globalFilterValue}
-          onChange={onFilter}
-          placeholder={t('global.dictionary.searchPosition') as string}
-        />
-      </span>
-      <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
-    </div>
-  );
 
   // // LOGICA PARA ABRIR LOS MODALES 1 SOLA VEZ
   // const showDetails = (vacancy: IVacancy) => {
@@ -100,7 +205,8 @@ function ScheduleView() {
   //   setVisibleDeleteConfirm(true);
   // };
 
-  const dataviewGridItem = (data: IVacancy) => {
+
+  const dataviewGridItem = (data: ISchedule) => {
     return (
       <div className="flex gap-4">
         {Object.entries(groupedProducts).map(([day, schedules]) => (
@@ -146,19 +252,83 @@ function ScheduleView() {
   };
 
   return (
+    // <div className="grid p-fluid">
+    //     <div className="col-12 md:col-12">
+    //       <div className="card">
+    //         <ul className="list-none p-0 m-0 flex align-items-center font-medium mb-3">
+    //           <li>
+    //             <span className="text-500 no-underline line-height-3">
+    //               {t('sidebar.home.label')}
+    //             </span>
+    //           </li>
+    //           <li className="px-2">
+    //             <i className="pi pi-angle-right text-500 line-height-3" />
+    //           </li>
+    //           <li>
+    //             <span className="text-900 line-height-3">{t('sidebar.home.service')}</span>
+    //           </li>
+    //         </ul>
+    //         <div className="flex align-items-start flex-column lg:justify-content-between lg:flex-row">
+    //           <div>
+    //             <div>
+    //               <h5 className="font-medium text-3xl text-900">
+    //                 Estatus del Servicio Social{' '}
+    //                 <b className="text-primary">
+    //                   {StudentServiceStatusData && `- ${selectedStudentInfo?.fullName}`}
+    //                 </b>
+    //               </h5>
+    //             </div>
+    //           </div>
+    //         </div>
+
     <div className="grid">
       <div className="col-12">
         <div className="card">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex gap-2 flex-column">
+            <Controller
+              name="teacherName"
+              control={control}
+              rules={{ required: 'Campo Requerido' }}
+              render={({ field, fieldState }) => (
+                <>
+                  <label htmlFor={field.name}>
+                    Escribe el nombre completo o el número de control del alumno que deseas
+                    administrar
+                  </label>
+                  <AutoComplete
+                    field="fullName"
+                    value={field.value}
+                    onChange={(e) => field.onChange(e.value)}
+                    inputRef={field.ref}
+                    suggestions={filterstudents}
+                    completeMethod={search}
+                    itemTemplate={dataTemplate}
+                    className={classNames({ 'p-invalid': fieldState.error })}
+                    autoFocus
+                  />
+                  {errors.teacherName && (
+                    <small className="p-error">{errors.teacherName.message}</small>
+                  )}
+                </>
+              )}
+            />
+            <Button
+              type="submit"
+              label="Buscar"
+              severity="primary"
+              size="small"
+              className="p-button-sm p-2 p-1"
+            />
+          </form>
+        </div>
+        <div className="card">
           <h5>{t('global.dictionary.vacancyList')}</h5>
           <DataView
-            value={filteredValue || listValue}
+            value={filteredValue} //  || listValue
             layout={layout}
-            paginator
-            sortOrder={sortOrder}
-            sortField={sortField}
+            //sortField={sortField}
             itemTemplate={(data) => itemTemplate(data, layout, selectedStatus)}
             emptyMessage={String(t('global.dictionary.Novacancy'))}
-            header={dataViewHeader}
           />
         </div>
       </div>
