fe02f2cdb0fdc6ea026c00af2b158f7cf70b06f7
AngelDavidLA03 <angeldavid2k17@gmail.com>
2025-03-12 15:11:54 -0600

    asd

--- src/components/ui/justify/justifiesCrud.tsx
+++ src/components/ui/justify/justifiesCrud.tsx
@@ -13,6 +13,7 @@ import { dialogStore } from '../../../store/global/dialogStore';
 import { GRAPHQL_CLIENT } from '../../../utils/graphqlClient';
 import Addjustify from '../../forms/justify/dashbord/addjustify';
 import JustifyStatus from '../../forms/justify/dashbord/justifieStatus';
+import { useForm } from 'react-hook-form';
 
 interface ITeacherSearchResult {
   _id?: string | null | undefined;
